7dc4f420bd355017d7beb0809ab80d2a97215a1d
Austin <112780937+Alily223@users.noreply.github.com>
2023-03-02 00:29:45 -0500

    FINAL FINAL OF ALL FINALS

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
-const distPath = path.join(__dirname, 'dist');
+const distPath = path.join(__dirname, '/dist/');
 const indexPath = path.join(distPath, 'index.html');
 
 app.use(express.static(distPath));
 
-app.all('%PUBLIC_URL%/favicon.ico', (req, res) => res.status(204));
+app.all('/favicon.ico', (req, res) => res.status(204));
 
 app.get('*', (req, res) => {
   res.sendFile(indexPath);
