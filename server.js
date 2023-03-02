edaa069a9f5ea25002213fe7646c0ab1572de501
Austin <112780937+Alily223@users.noreply.github.com>
2023-03-01 21:29:44 -0500

    entirely rewritten server.js

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const path = require('path');
+const indexPath = path.join(__dirname, '/dist/index.html');
 
 app.use(express.static('public'));
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.use(express.static(__dirname + '/dist/'));
-app.get(/.*/, function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
-})
-app.listen(port);
 
-console.log("server started");
+app.get('*', (req, res) => {
+  res.sendFile(indexPath);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
