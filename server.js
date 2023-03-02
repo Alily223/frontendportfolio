fd2c300781553eca32b48ad8f29cc94f4e748c35
Austin <112780937+Alily223@users.noreply.github.com>
2023-03-01 22:07:52 -0500

    src code input

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 const path = require('path');
 const indexPath = path.join(__dirname, '/public/index.html');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname,'src', 'public')));
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 
