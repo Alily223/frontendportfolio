3815683dd35a552f2f77b893901e34a0b43dc650
Austin <112780937+Alily223@users.noreply.github.com>
2023-03-02 16:31:40 -0500

    console.logs for server.js

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
+console.log('triggering app.use(cors)')
+
 app.use(cors());
 
+console.log('triggering app.use /dist')
+
 app.use(express.static(path.join(__dirname, '/dist')));
+
+console.log('triggering applisten port')
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
