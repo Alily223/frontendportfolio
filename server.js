0aaba6f5757fd6b81d061798f922585c5df9aec6
Austin <112780937+Alily223@users.noreply.github.com>
2023-03-01 23:55:47 -0500

    hopefully final heroku commit

--- server.js
+++ server.js
@@ -2,17 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
-const distPath = path.join(__dirname, '/dist/');
-const indexPath = path.join(distPath, 'index.html');
 
-app.use(express.static(distPath));
+// Serve static files from the 'dist' folder
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.all('/favicon.ico', (req, res) => res.status(204));
-
-app.all('*', (req, res) => {
-  res.sendFile(indexPath);
+// Serve the 'index.html' file for all requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
