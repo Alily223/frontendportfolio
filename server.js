075cd53874f0b2f1070334a2e0b28aafdfcd876f
Austin <112780937+Alily223@users.noreply.github.com>
2023-03-01 21:34:42 -0500

    to public index

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
-const indexPath = path.join(__dirname, '/dist/index.html');
+const indexPath = path.join(__dirname, '/public/index.html');
 
 app.use(express.static('public'));
 
 app.get('/favicon.ico', (req, res) => res.status(204));
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/public/'));
 
 app.get('*', (req, res) => {
   res.sendFile(indexPath);
