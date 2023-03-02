d006df1e3ca4eb583a56b333cf6556899ff9fbb3
Austin <112780937+Alily223@users.noreply.github.com>
2023-03-02 00:03:03 -0500

    final test of the night

--- server.js
+++ server.js
@@ -2,16 +2,23 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
+const distPath = path.join(__dirname, '/dist');
+const indexPath = path.join(distPath, 'index.html');
 
-// Serve static files from the 'dist' folder
+app.use(express.static(distPath));
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Serve the 'index.html' file for all requests
-app.all('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.all('/favicon.ico', (req, res) => res.status(204));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Start the server
+app.all('*', (req, res) => {
+  res.sendFile(indexPath);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
