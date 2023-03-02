3f82b6ad768748f9fc92854a4946cf11c869bef0
Austin <112780937+Alily223@users.noreply.github.com>
2023-03-02 00:07:55 -0500

    I LIED

--- server.js
+++ server.js
@@ -2,20 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 const path = require('path');
-const distPath = path.join(__dirname, '/dist');
+const distPath = path.join(__dirname, 'dist');
 const indexPath = path.join(distPath, 'index.html');
 
 app.use(express.static(distPath));
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
 app.all('/favicon.ico', (req, res) => res.status(204));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-app.all('*', (req, res) => {
   res.sendFile(indexPath);
 });
 
