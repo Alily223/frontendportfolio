a100d6a2a6cd560a4cc31cd46a641d73256341c6
Austin <112780937+Alily223@users.noreply.github.com>
2023-03-01 21:00:56 -0500

    updated file trying to get favicon to not crash

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
+
+app.use(express.static('public'));
+
+app.get('/favicon.ico', (req, res) => res.status(204));
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
