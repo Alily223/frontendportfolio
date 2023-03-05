9fd9fd0989d1d3eeb32034af74bffc683fb2f08b
Austin <112780937+Alily223@users.noreply.github.com>
2023-03-04 20:44:00 -0500

    hopefully this works

--- src/Components/HomeFeatures/HomeDashboard.jsx
+++ src/Components/HomeFeatures/HomeDashboard.jsx
@@ -9,7 +9,7 @@ const HomeDashboard = () => {
 
   const getAllBlogs = async () => {
     try {
-        const response = await fetch("http://127.0.0.1:5000/blog/getblogs");
+        const response = await fetch("https://backendforlilygrenportfolio.herokuapp.com/blog/getblogs");
         const data = await response.json();
         setBlogs(data);
       } catch (error) {
--- src/Components/Pages/blog.jsx
+++ src/Components/Pages/blog.jsx
@@ -150,7 +150,7 @@ const Blog = () => {
     } else {
       const fetchBlogs = async () => {
         try {
-          const response = await fetch("http://127.0.0.1:5000/blog/getblogs");
+          const response = await fetch("https://backendforlilygrenportfolio.herokuapp.com/blog/getblogs");
           const data = await response.json();
           setBlogs(data);
           setIsLoading(false);
