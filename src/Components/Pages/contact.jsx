e04ae364755d23f1fb45dfb1c612c30f8a09ba2d
Austin <112780937+Alily223@users.noreply.github.com>
2023-03-06 03:37:16 -0500

    all pages done new styles and perfect pages

--- src/Components/Pages/contact.jsx
+++ src/Components/Pages/contact.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  });
+
+  const { name, email, subject, message } = formData;
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const mailtoLink = `mailto:a.lilygren@icloud.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`)}`;
+    window.location.href = mailtoLink;
+  };
+
   return (
     <div className="Page-Wrapper">
-      Contact
+      <div className="Contact-Page-Wrapper">
+        <h2>Contact Me From Here</h2>
+        <div className="Email-Form-Wrapper">
+        <form onSubmit={handleSubmit}>
+
+          <div className="Name-Wrapper">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" value={name} onChange={handleChange} />
+          </div>
+
+          <div className="Email-Wrapper">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" value={email} onChange={handleChange} />
+          </div>
+
+          <div className="Subject-Wrapper">
+            <label htmlFor="subject">Subject:</label>
+            <input type="text" id="subject" name="subject" value={subject} onChange={handleChange} />
+          </div>
+
+          <div className="Message-Wrapper">
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" value={message} onChange={handleChange}></textarea>
+          </div>
+          
+          <button type="submit">Send</button>
+        </form>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/Pages/hireforwork.jsx
+++ src/Components/Pages/hireforwork.jsx
@@ -1,9 +1,91 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Hireforwork = () => {
+  const navigate = useNavigate();
   return (
     <div className="Page-Wrapper">
-      Hireforwork
+      <div className="Hire-For-Work-Page-Wrapper">
+        <div className="Commission-A-Site">
+
+          <div className="Types-Of-Sites-To-Commission">
+            <h2>Types of Websites to Commission</h2>
+            <ul>
+              <li>Blog Website</li>
+              <li>Buisness Website</li>
+              <li>E-commerce Website</li>
+              <li>Portolio Website</li>
+              <li>Educational Website</li>
+              <li>Social Network Website</li>
+              <li>News Website</li>
+              <li>Magazine Website</li>
+              <li>Forum Website</li>
+              <li>Community Website</li>
+              <li>Gaming Website</li>
+              <li>Humor Website</li>
+              <li>Music Website</li>
+              <li>Art Website</li>
+              <li>Photography Website</li>
+              <li>Travel Website</li>
+              <li>Food Website</li>
+              <li>ETC...</li>
+            </ul>
+          </div>
+
+          <div className="Availability-Schedule">
+            <h2>My Availabilty</h2>
+            <p>I'll take on a maximum of four projects at a time one small, one medium, one large, and one extra.</p>
+            <div className="Slots-Wrapper">
+
+              <div className="Slot">
+                <div className="Title-Of-Slot">
+                  <h3>Small Slot</h3>
+                </div>
+                
+                <div className="Availability-For-Slot">
+                  <p>Open</p>
+                </div>
+              </div>
+              
+              <div className="Slot">
+                <div className="Title-Of-Slot">
+                  <h3>Medium Slot</h3>
+                </div>
+                
+                <div className="Availability-For-Slot">
+                  <p>Open</p>
+                </div>
+              </div>
+              
+              <div className="Slot">
+                <div className="Title-Of-Slot">
+                  <h3>Large Slot</h3>
+                </div>
+                
+                <div className="Availability-For-Slot">
+                  <p>Open</p>
+                </div>
+              </div>
+              
+              <div className="Slot">
+                <div className="Title-Of-Slot">
+                  <h3>Extra Slot</h3>
+                </div>
+                
+                <div className="Availability-For-Slot">
+                  <p>Open</p>
+                </div>
+              </div>
+
+            </div>
+            
+          </div>
+
+          <div className="Contact-Me-About-Hiring">
+            <button type="button" onClick={() => navigate("/contact")}>CONTACT ME</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
